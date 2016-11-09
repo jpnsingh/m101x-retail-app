d4a95d67708832f4a03b2c855fc88cb24b7bc1f8
jpnsingh <jpnsingh86@gmail.com>
2016-11-09 11:50:39 +0530

    [2016.11.09] - Initial Commit;

---
+++ src/schema/product.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    var mongoose = require('mongoose'),
+        Category = require('./category');
+
+    var productSchema = {
+        name: {
+            type: String,
+            required: true
+        },
+        pictures: [
+            {
+                type: String,
+                match: /^http:\/\//i
+            }
+        ],
+        price: {
+            amount: {
+                type: Number,
+                required: true
+            },
+            currency: {
+                type: String,
+                enum: ['USD', 'EUR', 'GBP'],
+                required: true
+            }
+        },
+        category: Category.categorySchema
+    };
+
+    module.exports = new mongoose.Schema(productSchema);
+    module.exports.productSchema = productSchema;
+})();
